726b9380aefc342596425ac393c8cb14b754b899
Bancharka <olipet101@gmail.com>
2024-11-07 09:29:18 +0100

    .

--- OdenseZooVue/vite.config.js
+++ OdenseZooVue/vite.config.js
@@ -1,10 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/odensezoo/' : '/',
   plugins: [
     vue(),
   ],
@@ -13,4 +12,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
