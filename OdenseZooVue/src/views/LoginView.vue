62155392ee9e0786f1093bf43d9a8f7721f673c4
Bancharka <olipet101@gmail.com>
2024-11-07 10:00:22 +0100

    Update LoginView.vue

--- OdenseZooVue/src/views/LoginView.vue
+++ OdenseZooVue/src/views/LoginView.vue
@@ -1,6 +1,9 @@
 <script setup>
+// Bruges til at lave reaktive data, bliver brugt til email og password
 import { ref } from "vue"; 
+// Her importeres getauth og signin fra firebase for at kunne bruge disse funktioner i koden
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// Jeg importerer userouter for at kunne pushe brugeren til dashboard når de er logget ind 
 import { useRouter } from "vue-router";
 import { app } from '../firebase';
 
@@ -9,11 +12,11 @@ const password = ref("");
 const router = useRouter();
 const register = () => {
     signInWithEmailAndPassword(getAuth(app), email.value, password.value)
-        .then((data) => {
+        .then((data) => {  /* Hvis login info er korrekt, så consol logger jeg nedestående og bliver så redirected til dashboard  */
             console.log("Du er nu logget ind!");
             router.push('/dashboard');
         })
-        .catch((error) => {
+        .catch((error) => {  /* Ellers consol logger den en error kode, samt alerter den error kode   */
             console.log(error.code);
             alert(error.message);
         });
@@ -28,6 +31,7 @@ const register = () => {
             <p><input class="logininput" type="text" placeholder="Email" v-model="email"/></p>
             <p><input class="logininput" type="password" placeholder="Password" v-model="password"/></p>
             <p><button class="loginbutton" @click="register">Log ind</button></p>
+            <!-- Et link til registrerings siden -->
             <router-link id="registerbutton" class ="loginbutton" to="/register"> <p>Opret bruger</p> </router-link>
         </div>
     </div>
