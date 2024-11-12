8fb3dbaab1971ff995bf037aa8662942008273e8
Bancharka <olipet101@gmail.com>
2024-11-12 10:34:35 +0100

    billeder og indhold til fordele siden

--- OdenseZooVue/src/views/GoodiesView.vue
+++ OdenseZooVue/src/views/GoodiesView.vue
@@ -3,6 +3,49 @@
 </script>
 
 <template>
+<img id="chipmunk" src="../img/chipmunk.png" alt="">
+<h2>Dit årskort giver store fordele!</h2>
+<div class="fordelesection">
+    <h4>- Fri entré året rundt</h4>
+    <h4>- 10% rabat i butik og spisesteder</h4>
+    <p>enkelte varer undtaget</p>
+    <h4>- Ta' en ven med gratis</h4>
+    <p>Gælder den 20-21 januar, 13-14 april og 26-27 oktober 2024</p>
+</div>
 
-<h1>Fordele</h1>
+
+<div class="fordelesection">
+    <h3>Et gratis besøg i (indsæt billeder af aalbord, givskud og ree park)</h3>
+    <h3>50% rabat på entré:</h3>
+    <h4>Hos udenlandske ZOO's: </h4>
+    <p>
+        <span style="font-weight: bold;"> Sverige:</span> Borås Djurspark <br>
+        <span style="font-weight: bold;">Tyskland:</span> Hannover ZOO <br>
+        <span style="font-weight: bold;">Norge:</span> Dyreparken Kristianssand
+    </p>
+</div>
+
+<div class="fordelesection">
+    <h3>25%rabat på entré:</h3>
+</div>
+
+<div class="fordelesection">
+    <h3>15% rabat på entré:</h3>
+    <p>I 14 danske zoo's og akvarier som f.eks. : <br>
+    Knuthenborg Safaripark <br>
+    Kattegatcentret og <br>
+    Fjord og Bælt <span style="text-decoration: underline;">(se alle på daza.dk)</span>
+    </p>
+</div>
+
+<div class="fordelesection">
+    <h3>Det med småt</h3>
+    <p>
+        <span style="font-weight: bold;">Universe:</span> Ønsker du at besøge universe, skal du huske at booke billet på forhånd, <a href="https://ticket.universe.dk/da-DKK/rabat/saesonkortrabat?utm_source=homepage&utm_medium=odensezoo&utm_campaign=50procent">dette kan du gøre lige her</a> <br> <br>
+        <span style="font-weight: bold;">Egeskov:</span>Tilbuddet til egeskov gælder kun dagsbilletter inkl. slottet i sæsonen fra april til oktober og ikke særlige events. <br> <br>
+        <span style="font-weight: bold;">Boras Djurpark:</span>Ved besøg i Boras Djurpark skal der købes billet på forhånd via deres hjemmeside. Indtast koden 'OZ2024' ved check-out for at få rabatten. Rabatten er personlig og årskort skal kunne fremvises sammen med billet.
+    </p>
+</div>
+
+<img src="../img/monkey.png" alt="">
 </template>
